4d695b80ac0ba44a2275c525f466d9d6bcbc8422
Vladyslav Yerofieiev <ext-vladyslav.yerofieiev@here.com>
2017-11-07 15:49:33 +0200

    initial commit

---
+++ SwiftyObjectiveC/SwiftyObjectiveC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyObjectiveC.h
+//  SwiftyObjectiveC
+//
+//  Created by Yerofieiev, Vladyslav on 11/7/17.
+//  Copyright © 2017 Hell_Ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyObjectiveC.
+FOUNDATION_EXPORT double SwiftyObjectiveCVersionNumber;
+
+//! Project version string for SwiftyObjectiveC.
+FOUNDATION_EXPORT const unsigned char SwiftyObjectiveCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyObjectiveC/PublicHeader.h>
+
+
